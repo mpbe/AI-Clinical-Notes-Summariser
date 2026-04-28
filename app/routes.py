3bc0f0779e601b5f019be7c18cac5e74dec778a5
Mike <mbell85@ymail.com>
2026-04-28 13:34:46 +0100

    route updated to include AI response. HTML and CSS updated

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, redirect, url_for, flash
+from .services import ai_service
 
 main = Blueprint("main", __name__)
 
-@main.get("/")
+
+@main.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+
+    response = None
+
+    if request.method == "POST":
+
+        notes = request.form.get("notes", "").strip()
+        if not notes:
+            flash("please enter text in the box to continue", "error")
+            return redirect(url_for("main.index"))
+
+        response = ai_service.summarise_clinical_notes(notes=notes)
+
+    return render_template("index.html", response=response)
